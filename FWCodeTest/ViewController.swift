cdb82a5e4ad42176cdd34b1e25fbd017cabeef64
Christopher Alford <chrisvalford@me.com>
2020-05-26 12:25:35 +0200

    Initial Commit

---
+++ FWCodeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FWCodeTest
+//
+//  Created by Christopher Alford on 26.05.20.
+//  Copyright © 2020 anapp4that. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
